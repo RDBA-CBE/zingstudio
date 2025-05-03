86e20660609e4c2cc6d02386e37715452277aaa0
nsathiyakala <nsathiyakala6@gmail.com>
2025-05-03 18:06:47 +0530

    changes

--- app/(service)/HowDoesItWorkBrand.jsx
+++ app/(service)/HowDoesItWorkBrand.jsx
@@ -7,7 +7,7 @@ const HowDoesItWorkBrand = ({WhyChoose}) => {
         <div className='container-fluid'>
             <div className='row  px-3 px-lg-5 justify-content-center' >
                 <div className='col-12 mx-0 px-0 py-5' style={{background:"linear-gradient(to right, #984674, #4c48d0)",borderRadius:"30px"}}>
-                <div className="container-fluid" >
+                <div className="container" >
         <div className="row justify-content-center">
             <div className='text-center'>
             <h2 className='font-48 fw-bold text-white' style={{fontSize:"48px"}}>How does it work?</h2>
@@ -18,8 +18,9 @@ const HowDoesItWorkBrand = ({WhyChoose}) => {
         <div className=' row justify-content-center mt-5'>
             {WhyChoose.map((item)=>(
                 <div className="col-md-4 mb-4 our-process-box ">
-                <h2 className="our-process-number text-white">{item.id}</h2>
-                <h4 className="our-process-title text-white">{item.title} </h4>
+                {/* <h2 className="our-process-number text-white">{item.id}</h2> */}
+                <img src="/images/elementbg/2.png" alt="" style={{width:"100px"}}/>
+                <h4 className="our-process-title text-white mt-2">{item.title} </h4>
                 <p className="our-process-description text-white">
                 {item.description}
                 </p>
